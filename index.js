c1e6641436c1dea3a6ddf6e3cf2bb5ac475fa018
Anatoly Tsapkov <anatoly.tsapkov@gmail.com>
2018-10-26 23:56:33 -0400

    wamp server

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+
+const http = require('http')
 const express = require('express')
 const path = require('path')
+const FoxRouter = require('fox-wamp')
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let app = express()
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+
+let httpServer = http.createServer(app)
+httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+router = new FoxRouter()
+router.listenWAMP({server: httpServer, path: "/wss"})
